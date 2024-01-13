46bff57eed1dcd32481f44c411e9e7778521e291
alanmandriaza <alan.mandriaza@gmail.com>
2024-01-13 01:38:31 +0000

    ignorando

--- Back/utils/utils.py
+++
@@ -1,9 +0,0 @@
-from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException
-
-def obtener_valor(driver, xpath):
-    try:
-        elemento = driver.find_element(By.XPATH, xpath)
-        return elemento.text.strip()
-    except NoSuchElementException:
-        return "0"
